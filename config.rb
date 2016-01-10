2ce26aaff3fb890c3c8df2eea8cf7b73f9a77aa9
Yong Bakos <ybakos@humanoriented.com>
2016-01-10 10:53:05 -0600

    Deprecate Capfile in lieu of middleman-deploy. Update impress.js. Rename extensions.

--- config.rb
+++ config.rb
@@ -1,47 +1,5 @@
-###
-# Compass
-###
-
-# Susy grids in Compass
-# First: gem install compass-susy-plugin
-# require 'susy'
-
-# Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
-
-###
-# Page options, layouts, aliases and proxies
-###
-
-# Per-page layout changes:
-#
-# With no layout
 page "/music/index.html", :layout => false
 
-#
-# With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
-#
-# A path which all have the same layout
-# with_layout :admin do
-#   page "/admin/*"
-# end
-
-# Proxy (fake) files
-# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
-#   @which_fake_page = "Rendering a fake page with a variable"
-# end
-
-###
-# Helpers
-###
-
-# Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
-
-# Methods defined in the helpers block are available in templates
 helpers do
 
   def default_title
@@ -55,33 +13,29 @@ helpers do
 
 end
 
-set :css_dir, 'stylesheets'
-
-set :js_dir, 'javascripts'
-
-set :images_dir, 'images'
-
 set :haml, {:attr_wrapper => '"'}
 
-# Build-specific configuration
+configure :development do
+  activate :livereload
+end
+
 configure :build do
-  # For example, change the Compass output style for deployment
-  # activate :minify_css
+  # activate :relative_assets
+  # set :relative_links, true
+end
 
-  # Minify Javascript on build
-  # activate :minify_javascript
-
-  # Enable cache buster
-  # activate :cache_buster
-
-  # Use relative URLs
-  activate :relative_assets
-  set :relative_links, true
-  # Compress PNGs after build
-  # First: gem install middleman-smusher
-  # require "middleman-smusher"
-  # activate :smusher
-
-  # Or use a different image path
-  # set :http_path, "/Content/images/"
+# Deployment config (middleman-deploy)
+case ENV['TARGET'].to_s.downcase
+when 'production'
+  activate :deploy do |deploy|
+    deploy.deploy_method   = :rsync
+    deploy.host            = 'yongbakos.com'
+    deploy.path            = 'yongbakos.com'
+  end
+else
+  activate :deploy do |deploy|
+    deploy.deploy_method   = :rsync
+    deploy.host            = 'staging.yongbakos.com'
+    deploy.path            = 'yongbakos.com'
+  end
 end
